4ded9cd2e7ab81b1f1070d2ac50833fdd48bbe8d
dominhdang148 <myclone235@gmail.com>
2024-11-20 21:14:46 +0700

    add treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = { "java", "html", "css" },
+    auto_install = true,
+    hightlight = {
+      enable = true,
+    },
+  },
+}
